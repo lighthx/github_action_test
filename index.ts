9f6dd61f1e4f4a03080075b0e9983f9f3585b538
huaxin@xyd.cn <Xyd123456>
2020-05-18 16:50:13 +0800

    test action package

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
-import core from  '@actions/core';
-import github from '@actions/github';
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
 try {
-    const ha=core.getInput('ha');
+    const ha = core.getInput('ha');
     console.log(`test ${ha}`);
-    const xi='xi ni mei';
-    core.setOutput("xi",xi);
-}catch (e) {
+    const xi = 'xi ni mei';
+    core.setOutput("xi", xi);
+} catch (e) {
     core.setFailed(e.message)
 }
