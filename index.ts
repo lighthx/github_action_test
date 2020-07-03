f8e4e5f13f9899c2f8b0c8b613c8732cc23e9703
huaxin@xyd.cn <Xyd123456>
2020-07-03 16:53:08 +0800

    test output xi

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 
 try {
-    const ha = core.getInput('ha');
+    // const ha = core.getInput('ha');
     console.log(`test ${ha}`);
     const xi = 'xi ni mei';
     core.setOutput("xi", xi);
